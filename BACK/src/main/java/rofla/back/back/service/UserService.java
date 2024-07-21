77db29a1a1d73793c63100cb423e2c949560724b
Roflaff <kimsp0317@gmail.com>
2024-07-22 00:54:16 +0900

    암호화 삭제

--- BACK/src/main/java/rofla/back/back/service/UserService.java
+++ BACK/src/main/java/rofla/back/back/service/UserService.java
@@ -1,7 +1,6 @@
 package rofla.back.back.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import rofla.back.back.model.User;
 import rofla.back.back.repository.UserRepository;
@@ -12,16 +11,11 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
     // 회원가입
     public void saveUser(User users) {
         if (userRepository.findByUsername(users.getUsername()).isPresent()) {
             throw new IllegalArgumentException("동일한 아이디가 있는 유저가 존재 합니다.");
         }
-        // 암호화 후 저장
-        String password =  users.getPassword();
-        users.setPassword(bCryptPasswordEncoder.encode(password));
         userRepository.save(users);
     }
 
