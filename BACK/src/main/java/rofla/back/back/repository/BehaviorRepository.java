f2cc049986bf02664b1ceb6a479555da661149df
Roflaff <kimsp0317@gmail.com>
2024-07-21 22:38:08 +0900

    ksp

--- BACK/src/main/java/rofla/back/back/repository/BehaviorRepository.java
+++ BACK/src/main/java/rofla/back/back/repository/BehaviorRepository.java
@@ -1,7 +1,9 @@
 package rofla.back.back.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import rofla.back.back.model.Behavior;
 
+@Repository
 public interface BehaviorRepository extends JpaRepository<Behavior, Integer> {
 }
--- BACK/src/main/java/rofla/back/back/repository/OrderRepository.java
+++ BACK/src/main/java/rofla/back/back/repository/OrderRepository.java
@@ -1,7 +1,9 @@
 package rofla.back.back.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import rofla.back.back.model.Order;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 }
