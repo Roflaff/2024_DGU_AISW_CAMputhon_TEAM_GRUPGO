3ac81e28ae8f3181f949290bc26f8fd87e0dc0a7
Roflaff <kimsp0317@gmail.com>
2024-07-21 22:29:17 +0900

    ksp

--- BACK/src/main/java/rofla/back/back/repository/DiaryRepository.java
+++ BACK/src/main/java/rofla/back/back/repository/DiaryRepository.java
@@ -1,7 +1,9 @@
 package rofla.back.back.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import rofla.back.back.model.Diary;
 
+@Repository
 public interface DiaryRepository extends JpaRepository<Diary, Integer> {
 }
--- BACK/src/main/java/rofla/back/back/repository/OrderedFoodRepository.java
+++ BACK/src/main/java/rofla/back/back/repository/OrderedFoodRepository.java
@@ -1,8 +1,10 @@
 package rofla.back.back.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import rofla.back.back.model.Order;
 import rofla.back.back.model.OrderedFood;
 
+@Repository
 public interface OrderedFoodRepository extends JpaRepository<OrderedFood, Integer> {
 }
